4494a4c0863e855cf33763fcfbc35f845d4932fa
OleksiiKhitryk <sana22_khoi@student.ztu.edu.ua>
2022-11-09 22:24:08 +0200

    Change last one two message

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Some new text - 1");
 Console.WriteLine("Some new text - 2");
 Console.WriteLine("Some new text - 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Some new text in last row - 4");
+Console.WriteLine("Some new text in last row - 5");
